8b3dde396e4d8773003a9af2f9af5d687aa3ee0a
William Ennals <w.ennals@gmail.com>
2020-03-01 23:04:32 -0500

    Wired up routing in nodejs

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const geocodeRouter = require("./APIs/geocode/geocode");
 const weatherRouter = require("./APIs/weather-data/weather-data");
 require("dotenv").config();
 
+const path = require("path");
+
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -12,6 +14,14 @@ app.use(express.json());
 app.use(placesRouter);
 app.use(geocodeRouter);
 app.use(weatherRouter);
+app.use(
+  express.static(path.join(__dirname, "/weather-display/dist/weather-app"))
+);
 
-app.listen(port, () => {
+app.get("/", (req, res) => {
+  res.sendFile(
+    path.join(__dirname + "/weather-display/dist/weather-app/index.html")
+  );
 });
+
+app.listen(port, () => {});
