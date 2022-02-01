b52a0ff1045469f24c85cba5fe324c8916c60e46
William Ennals <w.ennals@gmail.com>
2022-02-01 14:22:22 -0500

    updated env vars and angular version update

--- APIs/weather-data/weather-data.js
+++ APIs/weather-data/weather-data.js
@@ -1,26 +1,27 @@
-const express = require("express");
-const axios = require("axios");
+const express = require('express');
+const axios = require('axios');
 
 const router = express.Router();
 
 router.get(
-  "/api/get-current-weather-data/:latitude/:longitude",
-  async (req, res) => {
-    try {
-      const latitude = req.params.latitude;
-      const longitude = req.params.longitude;
-      const key = process.env.WEATHER_DATA_API_KEY;
+	'/api/get-current-weather-data/:latitude/:longitude',
+	async (req, res) => {
+		try {
+			const latitude = req.params.latitude;
+			const longitude = req.params.longitude;
+			const key = process.env.WEATHER_DATA_API_KEY;
 
-      const weatherAPI = `https://api.darksky.net/forecast/${key}/${latitude},${longitude}?exclude=minutely`;
-      const axios_response = await axios.get(weatherAPI);
-      if (!axios_response) {
-        res.status(404).send();
-      }
-      const forecast = axios_response.data;
-      res.send(forecast);
-    } catch (error) {
-      res.status(500).send();
-    }
-  }
+			const weatherAPI = `https://api.darksky.net/forecast/${key}/${latitude},${longitude}?exclude=minutely`;
+			console.log(weatherAPI);
+			const axios_response = await axios.get(weatherAPI);
+			if (!axios_response) {
+				res.status(404).send();
+			}
+			const forecast = axios_response.data;
+			res.send(forecast);
+		} catch (error) {
+			res.status(500).send();
+		}
+	}
 );
 module.exports = router;
